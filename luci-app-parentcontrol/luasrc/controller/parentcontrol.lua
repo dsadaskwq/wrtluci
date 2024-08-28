f278e94de7785e363ce3784d5f7bd178dc344fc4
dsadaskwq <111348232+dsadaskwq@users.noreply.github.com>
2024-08-28 16:55:55 +0800

    Update parentcontrol.lua

--- luci-app-parentcontrol/luasrc/controller/parentcontrol.lua
+++ luci-app-parentcontrol/luasrc/controller/parentcontrol.lua
@@ -3,14 +3,14 @@ module("luci.controller.parentcontrol", package.seeall)
 function index()
     if not nixio.fs.access("/etc/config/parentcontrol") then return end
 
-        entry({"admin", "control"}, firstchild(), "Control", 44).dependent = false
-	local e = entry({"admin","control","parentcontrol"},firstchild(),_("Parent Control"),2)
+        entry({"admin", "services"}, firstchild(), "Control", 44).dependent = false
+	local e = entry({"admin","services","parentcontrol"},firstchild(),_("Parent Control"),2)
 	e.dependent=false
 	e.acl_depends = { "luci-app-parentcontrol" }
-	entry({"admin","control","parentcontrol","time"},cbi("parentcontrol/time"),_("Time Control"),1).leaf=true
-	entry({"admin", "control", "parentcontrol","weburl"}, cbi("parentcontrol/weburl"), _("Weburl Control"), 20).leaf = true
-        entry({"admin", "control", "parentcontrol","protocol"}, cbi("parentcontrol/protocol"), _("Protocol Control"), 30).leaf = true 
-	entry({"admin", "control", "parentcontrol","status"}, call("status")).leaf = true
+	entry({"admin","services","parentcontrol","time"},cbi("parentcontrol/time"),_("Time Control"),1).leaf=true
+	entry({"admin", "services", "parentcontrol","weburl"}, cbi("parentcontrol/weburl"), _("Weburl Control"), 20).leaf = true
+        entry({"admin", "services", "parentcontrol","protocol"}, cbi("parentcontrol/protocol"), _("Protocol Control"), 30).leaf = true 
+	entry({"admin", "services", "parentcontrol","status"}, call("status")).leaf = true
 end
 
 function status()
